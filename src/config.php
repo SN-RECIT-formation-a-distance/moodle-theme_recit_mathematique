8e6fee5cabd672daa468eabfa49976ae25d3cee8
Gustavo Bazzo <gcbazzo@gmail.com>
2019-12-17 09:29:44 -0500

    Ajout bannières
    La variable layouts est assignée dans le thème base recit

--- src/config.php
+++ src/config.php
@@ -34,7 +34,7 @@ $THEME->name = 'recit_mathematique';
 // This setting list the style sheets we want to include in our theme. Because we want to use SCSS instead of CSS - we won't        
 // list any style sheets. If we did we would list the name of a file in the /style/ folder for our theme without any css file      
 // extensions.                                                                                                                      
-$THEME->sheets = [];                                                                                                                
+$THEME->sheets[] = "recit-mathematique";                                                                                                            
  
 // This is a setting that can be used to provide some styling to the content in the TinyMCE text editor. This is no longer the      
 // default text editor and "Atto" does not need this setting so we won't provide anything. If we did it would work the same         
@@ -49,87 +49,6 @@ $THEME->parents = ['recit'];
 
 $THEME->parents_exclude_sheets[] = "recit";
 
-$THEME->sheets[] = "recit-mathematique";
-
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';  
-$THEME->requiredblocks = ''; 
-//$THEME->iconsystem = \core\output\icon_system::FONTAWESOME;
-$THEME->layouts = [
-    // Most backwards compatible layout without the blocks - this is the layout used by default.
-    'base' => array(
-        'file' => 'columns2.php',
-        'regions' => array(),
-    ),
-    // Standard layout with blocks, this is recommended for most pages with general information.
-    'standard' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    // Course page.
-    'course' => array(
-        'file' => 'course.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-        'options' => array('nofooter' => true, 'nonavbar' => false, 'langmenu' => true),
-    ),
-    'coursecategory' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-    ),
-    // Internal course modules page.
-    'incourse' => array(
-        'file' => 'incourse.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-        'options' => array('nofooter' => true, 'nonavbar' => false, 'langmenu' => true),
-    ),
-    // The site home page.
-    'frontpage' => array(
-        'file' => 'frontpage.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-        'options' => array('nofooter' => true, 'nonavbar' => true),
-    ),
-    // Server administration scripts.
-    'admin' => array(
-        'file' => 'columns2.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-		'options' => array('nofooter' => true),
-    ),
-    // My dashboard page.
-    'mydashboard' => array(
-        'file' => 'mydashboard.php',
-        'regions' => array('side-pre'),
-        'defaultregion' => 'side-pre',
-        'options' => array('nofooter' => true, 'nonavbar' => true, 'langmenu' => true),
-    ),
-    
-    'login' => array(
-        'file' => 'login.php',
-        'regions' => array(),
-        'options' => array('langmenu' => true),
-    ),
-    // No blocks and minimal footer - used for legacy frame layouts only!
-    'frametop' => array(
-        'file' => 'columns1.php',
-        'regions' => array(),
-        'options' => array('nofooter' => true, 'nocoursefooter' => true),
-    ),
-    // Used during upgrade and install, and for the 'This site is undergoing maintenance' message.
-    // This must not have any blocks, links, or API calls that would lead to database or cache interaction.
-    // Please be extremely careful if you are modifying this layout.
-    'maintenance' => array(
-        'file' => 'maintenance.php',
-        'regions' => array(),
-    ),
-    // Should display the content and basic headers only.
-    'print' => array(
-        'file' => 'columns1.php',
-        'regions' => array(),
-        'options' => array('nofooter' => true, 'nonavbar' => false),
-    )
-];                                                                                                
 
+$THEME->requiredblocks = ''; 
